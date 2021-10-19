8294dd68e9e5827108533d1b196ed2830193217f
valerio <valerio.setti@gmail.com>
2021-10-19 21:31:33 +0200

    replace official rpi kernel repo with my github version

--- setup-env.sh
+++ setup-env.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 git clone https://github.com/raspberrypi/tools tools
-git clone --depth=1 --branch rpi-4.19.y https://github.com/raspberrypi/linux
+git clone --branch fake-block-device git@github.com:valeriosetti/rpi-4.19.git linux
 docker build -t rpi-build --build-arg uid=$(id -u) --build-arg gid=$(id -u)  docker/.
